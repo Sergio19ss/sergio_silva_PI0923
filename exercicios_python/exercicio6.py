fd2d0ee52174373b159d29a3253e7e09566b94df
Sergio <sergio.silva.t0126460@edu.atec.pt>
2025-04-23 10:47:02 +0100

    continuação

--- exercicios_python/exercicio6.py
+++ exercicios_python/exercicio6.py
@@ -1,1 +1,10 @@
-coco = 0
+primos = []
+num = 2
+while len(primos) < 10:
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            break
+    else:
+        primos.append(num)
+    num += 1
+print(primos)
